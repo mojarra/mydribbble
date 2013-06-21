02469d190daffee5718a952ef68035ec3ab57b8a
Antonio Delfín Martínez <antonio.delfin@jobandatalent.com>
2013-06-21 20:09:40 +0200

    - Add initial empty rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mydribbble::Application.config.secret_token = '588ca1851458a0104b71f9cb83d84a543ddd69a0297d83eda8a0ca3112b770edc96f2e9940afc72b01584dd5baa2cd644144671ef414d6b109ab437e6887d7ec'
